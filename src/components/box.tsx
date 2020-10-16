6b5f077062da0a50a62a41779990e47c4565501f
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-16 19:22:19 +0300

    refactor: div -> button in Box

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Container = styled.button`
   display: inline-block;
   vertical-align: middle;
   width: 20px;
   height: 20px;
   margin: 5px;
+  padding: 0;
+  border: none;
+  border-radius: 2px;
 `;
 
 const Box = (props: any) => {
