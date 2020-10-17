7ae485c5854d5eb44a601a4aa0f68e87019064f3
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-17 03:20:01 +0300

    feat: use stylized tooltip in Box

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
+import Tooltip from '@reach/tooltip';
+import '@reach/tooltip/styles.css';
 
 const Container = styled.button`
   display: inline-block;
@@ -27,12 +29,23 @@ const Box = (props: any) => {
 
   return (
     <>
-      <Container
+      <Tooltip
+        label={props.title}
         style={{
-          backgroundColor: props.profit ? 'green' : 'red',
-          color: props.profit ? 'green' : 'red',
+          background: 'hsla(0, 0%, 0%, 0.75)',
+          color: 'white',
+          border: 'none',
+          borderRadius: '2px',
+          padding: '.25em .5em',
         }}
-      />
+      >
+        <Container
+          style={{
+            backgroundColor: props.profit ? 'green' : 'red',
+            color: props.profit ? 'green' : 'red',
+          }}
+        />
+      </Tooltip>
     </>
   );
 };
