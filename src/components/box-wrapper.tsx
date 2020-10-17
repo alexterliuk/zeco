e40e5665177e4484168ec6e6db5a2c69c7067155
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-18 02:33:39 +0300

    refactor: split companies and _ids into separate entities

--- src/components/box-wrapper.tsx
+++ src/components/box-wrapper.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Box from './box';
-import companies from '../data/companies/index';
+import companies, { companiesIds, CompaniesIds } from '../data/companies/index';
 
 const Container = styled.div`
   border: 1px solid lightgrey;
@@ -11,8 +11,7 @@ const Container = styled.div`
 `;
 
 const BoxWrapper = () => {
-  const boxes = companies._ids.map(k => {
-    // @ts-ignore
+  const boxes = companiesIds.map((k: CompaniesIds) => {
     const company: CompanyProfile = companies[k];
     const { name, id } = company;
     const profit = company.financials[2020].netProfit.quarters[1] > 0;
