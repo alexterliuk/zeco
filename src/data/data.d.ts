f160a603fb910bc7f45e76924cab0c60c3b271a4
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-17 03:22:55 +0300

    feat: model company data

---
+++ src/data/data.d.ts
@@ -0,0 +1,24 @@
+interface CompanyProfile {
+  name: string;
+  shortName: string;
+  EDRPOU: string;
+  location: string;
+  financials: {
+    [key: number]: Financials;
+  };
+}
+
+interface FinancialResult {
+  quarters: number[];
+  year: number;
+}
+
+interface Financials {
+  netProfit: FinancialResult;
+  netProfitMargin: FinancialResult;
+  ebitda: FinancialResult;
+  ebitdaMargin: FinancialResult;
+  netRevenue: FinancialResult;
+  revenueGrowth: FinancialResult;
+  profitGrowth: FinancialResult;
+}
