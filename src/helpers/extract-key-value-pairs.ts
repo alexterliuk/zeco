9814ca7fe988427a6ac91e2e9ae77367b548cfa9
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-20 02:40:36 +0300

    feat: add extractKeyValuePairs helper func

---
+++ src/helpers/extract-key-value-pairs.ts
@@ -0,0 +1,37 @@
+/**
+ * Extract entries from object and convert them to key-value pairs.
+ * @param {object} data
+ * @param {array} [ignoreKeys] - collection with keys inside
+ * @returns [{ key: string, value: any }, ...]
+ */
+function extractKeyValuePairs(
+  data: KeyValuePairsObj,
+  ignoreKeys: string[] = []
+): KeyValuePairs {
+  const keys = Object.keys(data);
+  const ignoreThis = ignoreKeys.reduce(
+    (acc: IgnoreKeys, k) => ((acc[k] = true), acc),
+    {}
+  );
+  const result = [];
+  let key = keys.pop();
+
+  while (key) {
+    if (!ignoreThis[key]) {
+      result.push({ key, value: data[key] });
+    }
+    key = keys.pop();
+  }
+
+  return result;
+}
+
+type IgnoreKeys = {
+  [key: string]: boolean;
+};
+type KeyValuePairsObj = {
+  [key: string]: any;
+};
+
+export type KeyValuePairs = Array<{ key: string; value: any }>;
+export default extractKeyValuePairs;
