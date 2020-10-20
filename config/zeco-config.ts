e02e93179c5f55cf90a4446667d27ee930261832
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-20 23:56:09 +0300

    feat: add zecoConfig for customizing of what data to show

---
+++ config/zeco-config.ts
@@ -0,0 +1,39 @@
+const zecoConfig: ZecoConfig = {
+  lang: 'uk',
+  financials: {
+    year: 2020,
+  },
+  showInCompanyPanel: {
+    regInfo: ['location'], // name not added, bec. it is always shown
+    finInfo: ['netProfit', 'profitGrowth', 'netRevenue', 'revenueGrowth'],
+  },
+};
+
+const updateZecoConfig = (pathSegments: string[] = [], value?: any): void => {
+  if (value === undefined) return;
+
+  let lastPathSegm = pathSegments[pathSegments.length - 1];
+  let configOption = zecoConfig;
+  let i = 0;
+
+  while (configOption) {
+    if (!pathSegments.hasOwnProperty(i) || i === pathSegments.length - 1) {
+      break;
+    }
+    configOption = configOption[pathSegments[i++]];
+  }
+
+  // TODO: add check whether new value type === orig value type
+  //       to avoid assigning not appropriate values
+
+  if (typeof configOption === 'object' && configOption.hasOwnProperty(lastPathSegm)) {
+    configOption[lastPathSegm] = value;
+  }
+};
+
+type ZecoConfig = {
+  [key: string]: any;
+}
+
+export { updateZecoConfig };
+export default zecoConfig;
