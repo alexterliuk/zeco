3a7cc1d74feadcd4f8fae33227b0ee6252679f06
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-21 21:42:27 +0300

    feat: add updateObject - standalone method to update any object

---
+++ src/helpers/update-object.ts
@@ -0,0 +1,29 @@
+import pickData from './pick-data';
+
+/**
+ * Go to given path and update value in object.
+ * @param {object} pathSegments - with keys
+ * @param {any} val
+ * @param {object} obj - obj where to make an update
+ */
+function updateObject(
+  pathSegments: string[],
+  val: any,
+  obj?: { [key: string]: any }
+): void {
+  if (val === undefined || typeof obj !== 'object') return;
+
+  const lastPathSegm = pathSegments[pathSegments.length - 1];
+  const singleKey = pathSegments.length === 1 && lastPathSegm;
+
+  if (singleKey) {
+    obj[singleKey] = val;
+  } else {
+    const picked = pickData(obj, pathSegments.slice(0, -1))[lastPathSegm];
+    if (typeof picked === 'object') {
+      picked[lastPathSegm] = val;
+    }
+  }
+}
+
+export default updateObject;
