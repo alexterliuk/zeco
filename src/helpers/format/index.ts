0e0707a6819788a6560762516ca1d7dde0bc3b35
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-25 14:32:14 +0200

    refactor: export all format functions from single file

---
+++ src/helpers/format/index.ts
@@ -0,0 +1,15 @@
+import format from './format';
+import formatNum from './format-num';
+import formatStr from './format-str';
+import launchFormattingNum from './launch-formatting-num';
+import setDefaultFormat from './set-default-format';
+import walkAndDo from './walk-and-do';
+
+export {
+  format,
+  formatNum,
+  formatStr,
+  launchFormattingNum,
+  setDefaultFormat,
+  walkAndDo,
+};
