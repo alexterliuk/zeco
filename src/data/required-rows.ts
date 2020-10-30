5692b64dc01e8cec76c700c12390f8fa21cd46c9
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-30 13:32:48 +0200

    feat: add requiredRows

---
+++ src/data/required-rows.ts
@@ -0,0 +1,26 @@
+const requiredRows = {
+  finZvit: [
+    { i: 0, name: 'colNames' },
+    { i: 1, name: 'netIncome' },
+    { i: 7, name: 'producedCost' },
+    { i: 9, name: 'grossProfit' },
+    { i: 10, name: 'grossLoss' },
+    { i: 40, name: 'netProfit' },
+    { i: 41, name: 'netLoss' },
+    { i: 52, name: 'salaryExpenses' },
+  ],
+  finCoeff: [
+    { i: 0, name: 'colNames' },
+    { i: 15, name: 'incomeGrowth' },
+    { i: 16, name: 'profitGrowth' },
+  ],
+  finBalance: [
+    { i: 0, name: 'colNames' },
+    { i: 5, name: 'assetsFixed' },
+    { i: 49, name: 'assetsCurrent' },
+    { i: 51, name: 'assetsTotalValue' },
+    { i: 63, name: 'equity' },
+  ],
+};
+
+export default requiredRows;
