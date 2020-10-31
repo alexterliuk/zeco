d1c285fcd24a37a70eb416815b0ecfbaadf5192f
Alex Terliuk <alex.terliuk@gmail.com>
2020-10-31 21:55:16 +0200

    feat: add companiesIds, excelDataTypes for parsing

---
+++ src/data/companies-ids.js
@@ -0,0 +1,6 @@
+const excelDataTypes = ['balance', 'fincoeff', 'zvit'];
+
+const companiesIds = ['ukrgidroenergo', 'naftogaz', 'turboatom', 'ukrenergo'];
+
+module.exports.excelDataTypes = excelDataTypes;
+module.exports.companiesIds = companiesIds;
