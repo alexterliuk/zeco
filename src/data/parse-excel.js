dcaa6f873684c53a55cfac82876b76dfdb22a620
Alex Terliuk <alex.terliuk@gmail.com>
2020-11-01 20:15:31 +0200

    feat: add preConvertToCompanies

--- src/data/parse-excel.js
+++ src/data/parse-excel.js
@@ -1,3 +1,4 @@
+'use strict';
 const fs = require('fs');
 const path = require('path');
 const readXlsxFile = require('read-excel-file');
@@ -5,9 +6,11 @@ const { companiesIds, excelDataTypes } = require('./companies-ids');
 const { makeRed } = require('./color-makers');
 
 async function parseExcelToJson() {
-  const parsedFileNames = [];
+  const pathsToParsed = [];
 
   for (const id of companiesIds) {
+    const parsedDataFileNames = [];
+
     for (const dataType of excelDataTypes) {
       const slug = `${id}-${dataType}`;
       const pathToFile = path.join(__dirname, `companies-excel/${slug}.xlsx`);
@@ -32,14 +35,55 @@ async function parseExcelToJson() {
         if (parsedExcel.length) {
           const _path = path.join(__dirname, `companies-parsed/${slug}.json`);
           fs.writeFileSync(_path, JSON.stringify(parsedExcel, null, 2));
-          parsedFileNames.push(slug);
+          parsedDataFileNames.push(`${slug}.json`);
         }
       } catch (e) {
         console.log(makeRed(`Crashed when working with ${pathToFile}'`));
         console.log(e);
       }
     }
+
+    pathsToParsed.push({ id, parsedDataFileNames });
   }
 
-  return parsedFileNames;
+  return pathsToParsed;
+}
+
+async function preConvertToCompanies() {
+  const pathsToParsed = await parseExcelToJson();
+  if (!pathsToParsed.length) return;
+
+  const fileNameEndings = `-(${excelDataTypes.join('|')}).json`;
+  const pattern = new RegExp(fileNameEndings);
+  const companiesJsonBundles = {};
+  const companiesJsonBundlesPaths = [];
+
+  for (const path of pathsToParsed) {
+    const id = path.id;
+    companiesJsonBundles[id] = {};
+    companiesJsonBundlesPaths.push([id, []]);
+
+    let fileName = path.parsedDataFileNames[0];
+    let i = 0;
+
+    while (fileName) {
+      const end = fileName.match(pattern).index;
+      const dataType = fileName.slice(end + 1, -5); //e.g. ...-zvit.json -> zvit
+      const pathToJson = `./companies-parsed/${fileName}`;
+      companiesJsonBundles[id][dataType] = require(pathToJson);
+
+      const bundlesPaths = companiesJsonBundlesPaths;
+      bundlesPaths[bundlesPaths.length - 1][1].push(dataType);
+
+      fileName = path.parsedDataFileNames[++i];
+    }
+  }
+
+  return companiesJsonBundlesPaths.reduce((acc, p) => {
+    acc.push({
+      companyId: p[0],
+      args: [companiesJsonBundles[p[0]], p[1]],
+    });
+    return acc;
+  }, []);
 }
