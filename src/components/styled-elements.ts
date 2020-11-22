9f4d0dbc9f924f7c2900eed813f4ec68f5206867
Alex Terliuk <alex.terliuk@gmail.com>
2020-11-22 20:28:18 +0200

    feat: add ButtonAsRow

--- src/components/styled-elements.ts
+++ src/components/styled-elements.ts
@@ -17,4 +17,24 @@ const Button = styled.button`
   }
 `;
 
-export { Button };
+const ButtonAsRow = styled.button`
+  display: block;
+  width: 100%;
+  text-align: left;
+  background: transparent;
+  border: 0;
+  padding: 0 8px;
+  cursor: pointer;
+
+  &:hover {
+    background: #f3f3f3;
+  }
+
+  &:focus,
+  &:active {
+    border: none;
+    outline: none;
+  }
+`;
+
+export { Button, ButtonAsRow };
