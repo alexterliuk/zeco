f53f550226ad7dfd2a85c7d5a01e064f0b1a3241
Alex Terliuk <alex.terliuk@gmail.com>
2020-11-26 18:09:40 +0200

    refactor(Search): store buttons in ref, add resetInputState, handleBlur

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react';
+import React, {
+  useRef,
+  useState,
+  ReactElement,
+  FunctionComponentElement,
+} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ButtonAsRow } from './styled-elements';
@@ -17,47 +22,90 @@ const Label = styled.label`
 
 const Input = styled.input`
   font-family: serif;
+  padding: 1px 2px;
 `;
 
 const FoundItems = styled.div`
   font-family: serif;
   background: #f9f9f9;
+  overflow-y: auto;
 `;
 
-const Search = ({ data, onClick, labelName, maxWidth = 0 }: SearchProps) => {
-  const [filteredData, filterData] = useState(data);
+// data consists of pop-down options, for each a button (ButtonAsRow) is created;
+// on a button's click, option.onClick is called (if exists), or Search's onClick
+// (if exists), or noop
+const Search = ({
+  data,
+  onClick,
+  labelName,
+  maxWidth = 0,
+}: SearchProps) => {
+  const inputRef = useRef(null);
+
+  const dataAndButtonsRef = useRef(
+    data.map(
+      (item, i): DataAndButtons => ({
+        ...item,
+        button: (
+          <ButtonAsRow
+            key={item.text}
+            onClick={() => {
+              resetInputState();
+              (item.onClick || onClick || (() => void 0))(item.id, item);
+            }}
+          >
+            {item.text}
+          </ButtonAsRow>
+        ),
+      })
+    )
+  );
+
+  const [filteredData, filterData] = useState(dataAndButtonsRef);
   const [pristine, setPristine] = useState(true);
 
-  const handleChange = (v: string): void => {
-    if (pristine) setPristine(false);
-    filterData(data.filter(item => v && item.text.includes(v)));
+  const resetInputState = () => {
+    const inputEl: unknown = inputRef.current;
+    if (inputEl instanceof HTMLInputElement) {
+      inputEl.value = '';
+      setPristine(true);
+    }
   };
 
-  const handleClick = (id: string | number, item: SearchItem) => {
-    (item.onClick || onClick || (() => void 0))(id, item);
+  const handleBlur = (
+    elCurrent: HTMLElement,
+    elRelated: EventTarget | null
+  ) => {
+    if (!elCurrent.contains(elRelated as Node)) {
+      resetInputState();
+    }
+  };
+
+  const handleChange = (v: string) => {
+    if (pristine) setPristine(false);
+    const curr = dataAndButtonsRef.current;
+    filterData({
+      current: !v ? [] : curr.filter(item => item.text.includes(v)),
+    });
   };
 
   const mw = maxWidth > 0 && maxWidth;
 
   return (
-    <Container style={{ maxWidth: mw || '' }}>
+    <Container
+      style={{ maxWidth: mw || '' }}
+      onBlur={e => handleBlur(e.currentTarget, e.relatedTarget)}
+    >
       {labelName ? <Label htmlFor="foundItem">{`${labelName}:`}</Label> : null}
       <Input
         type="text"
         name="foundItem"
+        ref={inputRef}
         style={{ width: mw ? `${mw - padding * 2}px` : '' }}
         onChange={e => handleChange(e.currentTarget.value)}
       />
       <FoundItems>
-        {(pristine ? [] : filteredData).map(item => (
-          <ButtonAsRow
-            tabIndex={0}
-            key={item.text}
-            onClick={() => handleClick(item.id, item)}
-          >
-            {item.text}
-          </ButtonAsRow>
-        ))}
+        {pristine ? [] : filteredData.current.map(item => item.button)}
       </FoundItems>
     </Container>
   );
@@ -90,5 +138,9 @@ interface SearchItem {
 }
 
 type SearchOnClick = (id: string | number, item?: SearchItem) => unknown;
+
+interface DataAndButtons extends SearchItem {
+  button: FunctionComponentElement<ReactElement>;
+}
 
 export default Search;
