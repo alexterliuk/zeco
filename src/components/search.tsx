1773bbf6bcd9c320ba9a381818c2989d613a9ccc
Alex Terliuk <alex.terliuk@gmail.com>
2020-11-26 21:47:52 +0200

    feat(Search): handle keydown - Up, Down, Enter, Esc

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -10,7 +10,10 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ButtonAsRow } from './styled-elements';
 
+// TODO; move backgroundColorInSearch to zecoConfig
+const backgroundColorInSearch = 'lightblue';
 const padding = 5;
+
 const Container = styled.div`
   border-radius: 8px;
   box-shadow: 0px 0px 8px 1px #d4d9dc;
@@ -86,6 +89,12 @@ const Search = ({
           </ButtonAsRow>
         ),
         ref: buttonsRefs[i],
+        selected: false,
+        // declare onClick here to handle Enter keydown
+        onClick: () => {
+          resetSearchState();
+          (item.onClick || onClick || (() => void 0))(item.id, item);
+        }
       })
     )
   );
@@ -105,11 +114,13 @@ const Search = ({
 
   const [filteredData, filterData] = useState(dataAndButtonsRef);
   const [pristine, setPristine] = useState(true);
+  const [selectedItemIdx, setSelectedItemIdx] = useState(-1);
 
   const resetSearchState = () => {
     const inputEl: unknown = inputRef.current;
     if (inputEl instanceof HTMLInputElement) {
       inputEl.value = '';
+      setSelectedItemIdx(-1);
       setPristine(true);
     }
   };
@@ -125,10 +136,80 @@ const Search = ({
 
   const handleChange = (v: string) => {
     if (pristine) setPristine(false);
+    // reset idx - if up/down arrow key pressed,
+    // it'll select last/first item from new found items
+    if (selectedItemIdx !== -1) setSelectedItemIdx(-1);
     const curr = dataAndButtonsRef.current;
     filterData({
       current: !v ? [] : curr.filter(item => item.text.includes(v)),
     });
+  };
+
+  const handleKeyDown = (key: string) => {
+    if (key === 'ArrowDown' || key === 'ArrowUp') {
+      const DOWN = key === 'ArrowDown';
+      const filtData = filteredData.current;
+      const selIdx = selectedItemIdx;
+      let idx: number;
+      if (DOWN) {
+        idx = selIdx === filtData.length - 1 ? 0 : selIdx + 1;
+      } else {
+        idx = selIdx < 1 ? filtData.length - 1 : selIdx - 1;
+      }
+      const foundItem = filtData[idx];
+      const firstFoundItem = !idx;
+      const lastFoundItem = idx === filtData.length - 1;
+
+      const el: unknown = foundItem.ref.current;
+      if (el instanceof HTMLElement) {
+        dataAndButtonsRef.current.map((dnb, i) => {
+          if (dnb.text === foundItem.text) {
+            const el: unknown = dnb.ref.current;
+            if (el instanceof HTMLElement) {
+              el.style.background = backgroundColorInSearch;
+              foundItem.selected = true;
+              setSelectedItemIdx(idx);
+
+              const fi: unknown = foundItemsRef.current;
+              if (fi instanceof HTMLElement) {
+                if (DOWN) {
+                  if (firstFoundItem) {
+                    // scroll to first item
+                    fi.scrollBy(0, 0 - foundItemsMaxHeight);
+                  } else if (i > maxItemsInFoundItems) {
+                    fi.scrollBy(0, buttonHeight);
+                  }
+                } else {
+                  // UP
+                  if (lastFoundItem) {
+                    // scroll to last item
+                    fi.scrollBy(0, foundItemsMaxHeight);
+                  } else if (i < filtData.length - (maxItemsInFoundItems - 1)) {
+                    fi.scrollBy(0, 0 - buttonHeight);
+                  }
+                }
+              }
+            }
+          } else {
+            dnb.selected = false;
+            const el: unknown = dnb.ref.current;
+            if (el instanceof HTMLElement) {
+              el.style.background = '';
+            }
+          }
+        });
+      }
+    }
+
+    if (key === 'Enter') {
+      const item = filteredData.current.filter(obj => obj.selected)[0];
+      if (item) item.onClick();
+      resetSearchState();
+    }
+
+    if (key === 'Escape') {
+      resetSearchState();
+    }
   };
 
   const mw = maxWidth > 0 && maxWidth;
@@ -146,6 +227,7 @@ const Search = ({
         ref={inputRef}
         style={{ width: mw ? `${mw - padding * 2}px` : '' }}
         onChange={e => handleChange(e.currentTarget.value)}
+        onKeyDown={e => handleKeyDown(e.key)}
       />
       <FoundItems
         ref={foundItemsRef}
@@ -190,6 +272,8 @@ type SearchOnClick = (id: string | number, item?: SearchItem) => unknown;
 interface DataAndButtons extends SearchItem {
   button: FunctionComponentElement<ReactElement>;
   ref: MutableRefObject<null>;
+  selected: boolean;
+  onClick: () => void;
 }
 
 export default Search;
