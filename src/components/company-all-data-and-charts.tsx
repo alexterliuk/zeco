5bcf905e66480552a981495406ca72bcec9895ee
Alex Terliuk <alex.terliuk@gmail.com>
2020-12-08 01:24:25 +0200

    feat: add charts to CompanyAllDataAndCharts

--- src/components/company-all-data-and-charts.tsx
+++ src/components/company-all-data-and-charts.tsx
@@ -1,18 +1,102 @@
 import React from 'react';
-import { CompanyId } from '../translations/translations';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import zecoConfig from '../../config/zeco-config';
+import { CompanyId, Language } from '../translations/translations';
 import getCompanyAllDataPanel from './company-all-data-panel';
+import getChartsData from '../helpers/get-charts-data';
+import { BarCharts } from './bar-charts';
+import {
+  ChartSpecTranslations,
+  translateCommon,
+} from '../translations/translate';
+
+const Preamble = styled.p`
+  display: flex;
+`;
+
+const Message = styled.span`
+  margin-left: auto;
+`;
+
+const PanelWrapper = styled.div`
+  margin-bottom: 1.45rem;
+`;
 
 const CompanyAllDataAndCharts = ({
   companyId,
 }: {
   companyId: CompanyId | string;
 }) => {
-  return companyId ? (
+  if (!companyId) return null;
+  const composedCompanyAllDataPanel = getCompanyAllDataPanel(companyId);
+  const data = composedCompanyAllDataPanel?.props;
+  const chartsData = data && getChartsData(data);
+  const charts =
+    chartsData &&
+    ((keysPairs: [string, number][]) =>
+      keysPairs.reduce(
+        (acc: { [key: string]: ChartSpecTranslations }, keysPair) => {
+          const [k1, k2] = [keysPair[0], keysPair[1]];
+          acc[k1] = (chartsData[k2][k1] as unknown) as ChartSpecTranslations;
+          return acc;
+        },
+        {}
+      ))(Object.entries(data.statementsIndicesInTbodyRows));
+
+  return (
     <>
-      {getCompanyAllDataPanel(companyId)}
-      {/* charts will be here */}
+      <Preamble>
+        <Message>{translateCommon('dataGivenInKHryvnias')}</Message>
+      </Preamble>
+      <PanelWrapper>{composedCompanyAllDataPanel}</PanelWrapper>
+      {chartsData ? (
+        <>
+          <BarCharts
+            initChartSpec={charts.netProfit[getLang()]}
+            chartsSpecs={[
+              charts.profitGrowth[getLang()],
+              charts.grossProfit[getLang()],
+            ]}
+          />
+          <BarCharts
+            initChartSpec={charts.netLoss[getLang()]}
+            chartsSpecs={[charts.grossLoss[getLang()]]}
+          />
+          <BarCharts
+            initChartSpec={charts.netIncome[getLang()]}
+            chartsSpecs={[charts.incomeGrowth[getLang()]]}
+          />
+          <BarCharts
+            initChartSpec={charts['assets.totalValue'][getLang()]}
+            chartsSpecs={[
+              charts['assets.current'][getLang()],
+              charts['assets.fixed'][getLang()],
+            ]}
+          />
+          <BarCharts
+            initChartSpec={charts.equity[getLang()]}
+            chartsSpecs={[
+              charts.producedCost[getLang()],
+              charts.salaryExpenses[getLang()],
+            ]}
+          />
+          {/*<BarCharts*/}
+          {/*  initChartSpec={charts.ebitda[getLang()]}*/}
+          {/*  chartsSpecs={[*/}
+          {/*    charts.ebitdaMargin[getLang()],*/}
+          {/*  ]}*/}
+          {/*/>*/}
+        </>
+      ) : null}
     </>
-  ) : null;
+  );
+};
+
+const getLang = () => zecoConfig.getItem(['lang']) as Language;
+
+CompanyAllDataAndCharts.propTypes = {
+  companyId: PropTypes.string,
 };
 
 export default CompanyAllDataAndCharts;
