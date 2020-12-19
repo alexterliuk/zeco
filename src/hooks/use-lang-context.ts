138c7b40a91fc4286b52a87fb7e8863089bf7b16
Alex Terliuk <alex.terliuk@gmail.com>
2020-12-20 00:23:52 +0200

    feat: add useLangContext hook

---
+++ src/hooks/use-lang-context.ts
@@ -0,0 +1,44 @@
+import zecoConfig from '../../config/zeco-config';
+import { Language } from '../translations/translations';
+
+/**
+ * Singleton which stores current lang to share it between components.
+ */
+const useLangContext = (() => {
+  const langContext = {
+    lang: zecoConfig.getItem(['lang']),
+  };
+
+  const _subscribers: Subscriber[] = [];
+
+  const api = {
+    subscribe: (updater: Subscriber) => {
+      if (!_subscribers.includes(updater)) _subscribers.push(updater);
+    },
+    unsubscribe: (updater: Subscriber) => {
+      let idx = -1;
+      void _subscribers.some((s, i) =>
+        s === updater ? ((idx = i), true) : false
+      );
+      if (idx > -1) _subscribers.splice(idx, 1);
+    },
+    updateDependentStates: (lang: Language) => {
+      _subscribers.forEach(s => {
+        langContext.lang = lang;
+        if (s.triggerTranslating) s.triggerTranslating();
+        if (s.updateLangAttr) s.updateLangAttr();
+      });
+    },
+    getLang: () => langContext.lang,
+  };
+
+  return { ...api };
+})();
+
+interface Subscriber {
+  id: string;
+  triggerTranslating?: () => void;
+  updateLangAttr?: () => void;
+}
+
+export default useLangContext;
