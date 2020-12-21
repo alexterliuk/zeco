2e5c5a1ae0500b7e2fe4c325d37a90b45ce9ca8b
Alex Terliuk <alex.terliuk@gmail.com>
2020-12-22 01:08:06 +0200

    chore: remove accidental brace

--- src/components/companies-all-data-section.tsx
+++ src/components/companies-all-data-section.tsx
@@ -23,7 +23,7 @@ const CompaniesAllDataSection = () => {
   return (
     <>
       <CheckboxControl checkboxSettings={useShowSettings} />
-      <Container>{companiesRows}</Container>)
+      <Container>{companiesRows}</Container>
     </>
   );
 };
