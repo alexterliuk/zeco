91cf593d10d798e2eb02bd6ba37eca1835e161ef
Alex Terliuk <alex.terliuk@gmail.com>
2020-12-24 22:34:30 +0200

    chore: edit text in 404 page, add translations

---
+++ src/pages/404.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { translateCommon } from '../translations/translate';
+import useLangContext from '../hooks/use-lang-context';
+import LinkToHomepage from '../components/link-to-homepage';
+
+const NotFoundPage = () => {
+  const [NOT_USED, triggerTranslating] = useState('');
+
+  useEffect(() => {
+    const translatingUpdater = {
+      id: '404 Page',
+      triggerTranslating: () => {
+        triggerTranslating(() => useLangContext.getLang());
+      },
+    };
+    useLangContext.subscribe(translatingUpdater);
+    return () => {
+      useLangContext.unsubscribe(translatingUpdater);
+    };
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <LinkToHomepage />
+      <h1>404 :(</h1>
+      <p>{translateCommon('404Page')}</p>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
